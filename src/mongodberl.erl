4cdd978a2dc91680ebab7caf8ac3e9982f053a10
Liu.Bo <bliu_923@hotmail.com>
2015-07-22 14:59:48 +0800

    init.
    Signed-off-by: Liu.Bo <bliu_923@hotmail.com>

---
+++ src/mongodberl.erl
@@ -0,0 +1,94 @@
+%%%-------------------------------------------------------------------
+%%% @author yunba
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. Jul 2015 5:58 PM
+%%%-------------------------------------------------------------------
+-module(mongodberl).
+-author("yunba").
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/1, get_seckey/2]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+get_seckey(PoolPid, Appkey) ->
+	execute(PoolPid, {get_seckey, Appkey}).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link(Args :: term()) ->
+	{ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link(Args) ->
+	supervisor:start_link({local, ?SERVER}, ?MODULE, [Args]).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+	{ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
+		MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
+		[ChildSpec :: supervisor:child_spec()]
+	}} |
+	ignore |
+	{error, Reason :: term()}).
+init([Args]) ->
+	{PoolName, MongoDbHost, MongoDbPort, MongoDbDatabase, MongodbConnNum} = Args,
+	RestartStrategy = one_for_one,
+	MaxRestarts = 1000,
+	MaxSecondsBetweenRestarts = 3600,
+
+	SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+	Pools = [
+		{PoolName, [
+			{size, MongodbConnNum},
+			{max_overflow, 30}
+		],
+			[{MongoDbHost, MongoDbPort, MongoDbDatabase}]
+		}
+	],
+
+	PoolSpecs = lists:map(fun({Name, SizeArgs, WorkerArgs}) ->
+		PoolArgs = [{name, {local, Name}},
+			{worker_module, erest_mongodb_worker}] ++ SizeArgs,
+		poolboy:child_spec(Name, PoolArgs, WorkerArgs)
+	end, Pools),
+
+	{ok, {SupFlags, PoolSpecs}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+execute(PoolPid, Cmd) ->
+	poolboy:transaction(PoolPid, fun(Worker) ->
+		gen_server:call(Worker, Cmd)
+	end).
+
+
---
+++ src/mongodberl_worker.erl
@@ -0,0 +1,168 @@
+%%%-------------------------------------------------------------------
+%%% @author yunba
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. Jul 2015 7:00 PM
+%%%-------------------------------------------------------------------
+-module(mongodberl_worker).
+-author("yunba").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1,
+	handle_call/3,
+	handle_cast/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {
+	mongodb_pid
+}).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link(Args :: term()) ->
+	{ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link(MongoInfo) ->
+	gen_server:start_link(?MODULE, [MongoInfo], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+	{ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+	{stop, Reason :: term()} | ignore).
+init([MongoInfo]) ->
+	[{Host, Port, Database}] = MongoInfo,
+	process_flag(trap_exit, true),
+	{ok, Connection} = mongo:connect(Host, Port, list_to_binary(Database)),
+	io:format("mongob init: ~p~n", [Connection]),
+	{ok, #state{mongodb_pid = Connection}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+	State :: #state{}) ->
+	{reply, Reply :: term(), NewState :: #state{}} |
+	{reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+	{noreply, NewState :: #state{}} |
+	{noreply, NewState :: #state{}, timeout() | hibernate} |
+	{stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+	{stop, Reason :: term(), NewState :: #state{}}).
+
+handle_call({get_seckey, Appkey}, _From, #state{mongodb_pid = Conn}=State) ->
+	{Doc} = mongo:find_one(Conn, <<"apps">>, {<<"_id">>, logic_util:to_bin(binary_to_list(Appkey))}),
+	Reply = case bson:lookup(seckey, Doc) of
+		         {Value} ->
+			         io:format("worker: ~p~n ~p~n", [Value, State#state.mongodb_pid]),
+			         {true, Value};
+		         _Else ->
+			         io:format("find seckey from mongodb failed ~p, ~p~n", [Appkey, _Else]),
+			         {false, _Else}
+	         end,
+	{reply, Reply, State};
+
+handle_call(_Request, _From, State) ->
+	{reply, ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_cast(Request :: term(), State :: #state{}) ->
+	{noreply, NewState :: #state{}} |
+	{noreply, NewState :: #state{}, timeout() | hibernate} |
+	{stop, Reason :: term(), NewState :: #state{}}).
+handle_cast(_Request, State) ->
+	{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
+	{noreply, NewState :: #state{}} |
+	{noreply, NewState :: #state{}, timeout() | hibernate} |
+	{stop, Reason :: term(), NewState :: #state{}}).
+handle_info(_Info, State) ->
+	io:format("handle info, ~p~n", [_Info]),
+	{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+	State :: #state{}) -> term()).
+terminate(_Reason, _State) ->
+	io:format("mongo terminate~n"),
+	ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
+	Extra :: term()) ->
+	{ok, NewState :: #state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
